2279b2ea18468022982f770da9e8c643c0d40093
RushiPokhare <rsp9725@gmail.com>
2025-02-22 02:16:44 +0530

    static method

---
+++ Methods/StaticMethod/Demo1.java
@@ -0,0 +1,11 @@
+package Methods.StaticMethod;
+
+public class Demo1 {
+    public static int add(int x ,int y){
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Demo1.add(2,3));
+    }
+}
