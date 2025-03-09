dd2fee890413f0a789cbf2983f0318df1eb813b0
RushiPokhare <rsp9725@gmail.com>
2025-03-09 15:27:46 +0530

    Access and array Problems

--- Array/Print_Reverse.java
+++ Array/Print_Reverse.java
@@ -1,7 +1,8 @@
 package Array;
-
+// Printing array in reverse order
 public class Print_Reverse {
     public static void main(String[] args) {
+
         int [] array1= {2,4,6,10,11,78};
 
         for (int index = array1.length-1; index >=0 ; index--) {
