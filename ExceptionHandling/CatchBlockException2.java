19f35480450a32fb1bdf5070b63c2fa701ebc068
RushiPokhare <rsp9725@gmail.com>
2025-03-31 01:21:00 +0530

    catch block Exception

---
+++ ExceptionHandling/CatchBlockException2.java
@@ -0,0 +1,23 @@
+package ExceptionHandling;
+// Refer NoteBook ---> Session 2
+public class CatchBlockException2 {
+    public static void main(String[] args) {
+       try {
+           try {
+               System.out.println(10 / 0);
+           } catch (ArithmeticException ae) {
+               System.out.println("inner Catch");
+               System.out.println(10/0);   //---> We Write the exception in the Catch block
+           }
+           System.out.println("Rest of the main-->outer try");
+       }catch(ArithmeticException ae){
+           System.out.println("outer catch");
+           System.out.println(10/0);
+       }
+        System.out.println("Rest of the main");
+    }
+}
+//output-->
+//          inner catch
+//          outer catch
+//          Arithmetic Exception
--- ExceptionHandling/InnerTry.java
+++ ExceptionHandling/InnerTry.java
@@ -15,8 +15,12 @@ public class InnerTry {
         System.out.println("Rest of the main");
     }
 }
+
+
 //output --> inner catch block
 //           outer try block
 //           Rest of the main
+
+//Exception is directly handled in inner try catch block exception not come into outer catch block because exception are already handled.
 
 //Explanation -->outer catch Block not printed because of..exception 10/2 is handle in inner catch block...
