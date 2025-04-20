2ac01603b0affd43488910edac5ba45501f53622
RushiPokhare <rsp9725@gmail.com>
2025-04-20 15:10:49 +0530

    get and set priority to the thread

---
+++ multithreading/M15_ThreadPriority.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class M15_ThreadPriority {
+    public static void main(String[] args) {
+        Thread thread=new Thread(()->{
+            System.out.println("This is a Custom Thread");
+        },"thread1");
+
+        thread.start();
+        thread.setPriority(10);  //for setting the thread priority
+
+        int priority=thread.getPriority();
+        System.out.println(priority); //for getting the thread priority
+    }
+}
