c9c895612fa93252aa9149c324641235ec9c3762
RushiPokhare <rsp9725@gmail.com>
2025-04-24 02:01:01 +0530

    linked list and methods

---
+++ collection/C14_LinkedList.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class C14_LinkedList {
+    public static void main(String[] args) {
+//        homo & Hetero ,null,duplicates,IO maintained
+        LinkedList linkedList=new LinkedList<>();
+        linkedList.add(2);
+        linkedList.add("Rushi");
+        linkedList.add("Rushi");
+        linkedList.add(20.0f);
+        linkedList.add(null);
+        linkedList.add(null);
+
+        System.out.println(linkedList);
+    }
+}
---
+++ collection/C15_LL_addAll.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.List;
+
+public class C15_LL_addAll {
+    public static void main(String[] args) {
+        List list=new LinkedList();
+        list.add(2);
+        list.add(7);
+        list.add(8);
+        list.add(5);
+
+        List list2=new LinkedList();
+        list2.add(120);
+        list2.add(500);
+
+        System.out.println("Added Success -->"+list.addAll(list2));
+        System.out.println(list);
+    }
+}
