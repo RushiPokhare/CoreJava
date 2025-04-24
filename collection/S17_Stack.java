ba7b3f912de44d97450647574dff335a093e1f46
RushiPokhare <rsp9725@gmail.com>
2025-04-24 17:01:57 +0530

    stack and its method

---
+++ collection/S17_Stack.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.Stack;
+
+public class S17_Stack {
+    public static void main(String[] args) {
+        Stack books=new Stack();
+        books.push("Shri Dattaguru Charitra");
+        books.push("Mahabharata");
+        books.push("Ramayana");
+        books.push("Rich Dad Poor Dad");
+
+        System.out.println(books);
+    }
+}
