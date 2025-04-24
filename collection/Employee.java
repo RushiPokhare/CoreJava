d5ef00893b1c0f278c2e79a5c9d4174551ae1361
RushiPokhare <rsp9725@gmail.com>
2025-04-24 23:57:27 +0530

    How to save custome objects into collections

---
+++ collection/Employee.java
@@ -0,0 +1,16 @@
+package collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Employee {
+    int id;
+    String name;
+    int salary;
+}
