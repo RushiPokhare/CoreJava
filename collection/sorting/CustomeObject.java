e553284e38327abfce7981d53cd386f2bbe77d89
RushiPokhare <rsp9725@gmail.com>
2025-04-26 01:26:40 +0530

    Custom Object sorting

---
+++ collection/sorting/CustomeObject.java
@@ -0,0 +1,22 @@
+package collection.sorting;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CustomeObject {
+    public static void main(String[] args) {
+            Student student1=new Student(1,"Sai","pune");
+            Student student2=new Student(67,"Rudra","Nashik");
+            Student student3=new Student(89,"Tushar","Thane");
+            Student student4=new Student(5,"Sahil","Mumbai");
+
+        List<Student> studentList=new LinkedList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.forEach(System.out::println);
+    }
+
+}
---
+++ collection/sorting/Student.java
@@ -0,0 +1,24 @@
+package collection.sorting;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Student implements Comparable<Student> {
+    int id;
+    String name;
+    String address;
+
+
+    @Override
+    public int compareTo(Student student) {
+        return this.id-student.id;  //<--Shorting Based on Student ID
+//        return this.name(compareTo(student));
+
+    }
+}
